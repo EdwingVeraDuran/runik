8799472deefc8c57ba053109a17ee9a687297ec0
Edwing Vera <edwingveraduran@gmail.com>
2025-07-25 00:23:19 -0500

    Define auth hooks and services

---
+++ src/features/auth/hooks/useSession.ts
@@ -0,0 +1,28 @@
+import { supabase } from "@/lib/supabase";
+import { Session } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+
+export function useSession() {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    const getSession = async () => {
+      const { data } = await supabase.auth.getSession();
+      setSession(data.session);
+    };
+
+    getSession();
+
+    const { data: listener } = supabase.auth.onAuthStateChange(
+      (_event, session) => {
+        setSession(session);
+      }
+    );
+
+    return () => {
+      listener.subscription.unsubscribe();
+    };
+  }, []);
+
+  return session;
+}
