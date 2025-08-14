ceea41410bb9978bc410e7176e1b4392c427ab5a
Edwing Vera <edwingveraduran@gmail.com>
2025-08-14 01:51:02 -0500

    Add CategoryTile component for product categories
    Introduces a CategoryTile component to display and edit product category names. Includes inline editing functionality and a dropdown menu for edit and delete actions.

---
+++ src/features/product-categories/components/CategoryTile.tsx
@@ -0,0 +1,71 @@
+import { Button } from "@/components/ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+import { ProductCategory } from "@/features/product-categories/types/category";
+import { EllipsisVertical } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
+
+type CategoryTileProps = {
+  category: ProductCategory;
+  onUpdate: (category: Omit<ProductCategory, "created_at">) => void;
+};
+
+export default function CategoryTile({ category, onUpdate }: CategoryTileProps) {
+
+  const [name, setName] = useState<string>(category.name);
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (isEditing) {
+      const timeout = setTimeout(() => {
+        inputRef.current?.focus();
+      }, 50);
+      return () => clearTimeout(timeout);
+    }
+
+  }, [isEditing]);
+
+  const handleSave = () => {
+    if (name.trim() !== "") {
+      setIsEditing(false);
+      onUpdate({
+        id: category.id,
+        name: name,
+      });
+    } else {
+      setIsEditing(false);
+      setName("");
+    }
+  }
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleSave();
+    } else if (e.key === "Escape") {
+      setIsEditing(false);
+      setName(category.name);
+    }
+  }
+
+  return (
+    <div className="flex justify-between items-center p-2 px-6 gap-4">
+      {isEditing ? (
+        <Input ref={inputRef} id="name_input" type="text" value={name} onChange={(e) => setName(e.target.value)} onBlur={handleSave} onKeyDown={handleKeyDown} />
+      ) : (
+        <span>{category.name}</span>
+      )}
+      <Button variant="outline" size="icon" className="size-7">
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <EllipsisVertical />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="start" onCloseAutoFocus={(e) => e.preventDefault()}>
+            <DropdownMenuItem onClick={() => setIsEditing(true)}>Editar</DropdownMenuItem>
+            <DropdownMenuItem>Eliminar</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </Button>
+    </div >
+  );
+}
